fbce5c034b66a6839acdeb94ba4c65da01eebb16
JustLailai <dianamilasan@gmail.com>
2019-10-06 15:59:02 +0200

    Project Upload

---
+++ ReportMe.Test/JiraCalls.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using ReportMe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Windows;
+using ReportMe.Backend;
+
+namespace ReportMe.Test
+{
+    [TestFixture]
+    class JiraCalls
+    {
+    [TestCase]
+    public void SuccesfulLogin()
+        {
+            string user = "<<General Users>>";
+            string pass = "<<General user pass>>";
+            string auth = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(user + ":" + pass));
+            var sut = new AllTheShinies();
+            Assert.That(sut.JRvalid(user).IsSuccessStatusCode);
+        }
+
+    }
+}
---
+++ ReportMe.Test/TestrailCalls.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using ReportMe.Backend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace ReportMe.Test
+{
+    [TestFixture]
+    public class TestrailCalls
+    {
+        [Test]
+        public void TestrailNotEmty()
+        {
+            string user = "<<General User>>";
+            string pass = "<<General user pass>>";
+            var sut = new Get();
+            Assert.That(sut.Trequest("get_project/", "44", user, pass) != null);
+           
+        }
+        [Test]
+        public void TestRailResponseType()
+        {
+            string user = "<<general user>>";
+            string pass = "<<General user pass>>";
+            var sut = new Get();
+            Assert.That(sut.Trequest("get_project/", "44", user, pass).GetType() == typeof(JObject));
+        }
+        [Test]
+        public void TestrailResponseUsername()
+        {
+            //incomplete username or typo
+            string user = "<<Incomplete General User>>";
+            string pass = "<<General user pass>>";
+            var sut = new Get();
+            Assert.That(sut.Trequest("get_project/", "44", user, pass) == null);
+            user = "!@#@$%$%$^&^*&(";
+            sut = new Get();
+            Assert.That(sut.Trequest("get_project/", "44", user, pass) == null);
+        }
+        [Test]
+        public void TestRailResponsePassword()
+        {
+            string user = "<<General User>>";
+            string pass = "<<Incomplete password>>";
+            var sut = new Get();
+            Assert.That(sut.Trequest("get_project/", "44", user, pass) == null);
+        }
+    }
+}
