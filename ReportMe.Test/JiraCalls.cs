dd7ed90e7cf1af45932a72e1c7d92fbca966534c
JustLailai <dianamilasan@gmail.com>
2020-03-22 18:30:38 +0100

    Add 2 jira unit tests
    Cover UbiPrio call

--- ReportMe.Test/JiraCalls.cs
+++ ReportMe.Test/JiraCalls.cs
@@ -22,5 +22,26 @@ namespace ReportMe.Test
             Assert.That(sut.JRvalid(user).IsSuccessStatusCode);
         }
 
+    [TestCase]
+     public void RetrieveId()
+        {
+            string user = "<<General Users>>";
+            string pass = "<<General user pass>>";
+            string jiraInstance = "19";
+            string auth = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(user + ":" + pass));
+            var sut = new AllTheShinies();
+            Assert.That(sut.GetUbiPrioId(jiraInstance, auth), Contains.Substring("custom").IgnoreCase );
+        }
+
+        [TestCase]
+        public void FailedRetrieveCustomId()
+        {
+            string user = "<<General Users>>";
+            string pass = "<<General user pass>>";
+            string jiraInstance = "da";
+            string auth = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(user + ":" + pass));
+            var sut = new AllTheShinies();
+            Assert.That(sut.GetUbiPrioId(jiraInstance, auth), Contains.Substring("n/a").IgnoreCase);
+        }
     }
 }
